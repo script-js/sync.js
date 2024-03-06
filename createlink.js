36358760591e038847082799429b083f4e401491
script-js <134544514+script-js@users.noreply.github.com>
2024-03-06 15:31:25 -0800

    Delete createlink.js

--- createlink.js
+++
@@ -1,5 +0,0 @@
-void(window.open("https://sync-js.pages.dev/backup?fromURL=" + encodeURI(location.href) + "&data=" + btoa(JSON.stringify(localStorage)).replaceAll("+","%2B")))
-
-window.open("https://sync-js.pages.dev/get?url=" + location.href);
-var sjsSYSjson = JSON.parse(prompt("Paste Data:"));
-Object.keys(sjsSYSjson).forEach(function (k) {localStorage.setItem(k,sjsSYSjson[k])})

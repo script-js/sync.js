178a6dd7bce0d50483b7aa5800061c2660cedcde
script-js <134544514+script-js@users.noreply.github.com>
2024-06-15 20:23:38 -0700

    Update drive.js

--- new/drive.js
+++ new/drive.js
@@ -24,6 +24,8 @@ async function initializeGapiClient() {
    var token = localStorage.getItem('gapi_token');
    if (token) {
       gapi.client.setToken(JSON.parse(token));
+   } else {
+      location.replace("login")
    }
    startup()
 }

0869adba38b0adabbe2c6fb6b6ebfc099ac5199c
script-js <134544514+script-js@users.noreply.github.com>
2024-06-19 09:25:40 -0700

    Update import.js

--- import.js
+++ import.js
@@ -2,8 +2,9 @@ var sjsInjection = document.createElement("div")
 sjsInjection.innerHTML = `
 <div style="display: none; /* Hidden by default */ position: fixed; /* Stay in place */ z-index: 1; /* Sit on top */ left: 0; top: 0; width: 100%; /* Full width */ height: 100%; /* Full height */ overflow: auto; /* Enable scroll if needed */ background-color: rgb(0,0,0); /* Fallback color */ background-color: rgba(0,0,0,0.4); /* Black w/ opacity */" id="sjsSetModal"><div style="margin: 15% auto; background: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); text-align: center; box-sizing: border-box; border-radius: 10px; width: 50%; border: 2px solid blue;padding: 20px;">
   <span style="color: red; float: right; font-size: 28px; font-weight: bold; cursor:pointer;" onclick="sjsSetModal.style.display = 'none'">&times;</span>
+  <img src="https://sync-js.pages.dev/icon.png" style="margin:auto">
   <button onclick='sjs.backUp()' style="transition: ease 150ms; background: #46b063; border: none; border-radius: 3px; width: 500px; height: 50px;">Back Up</button>
-  <p></p>
+  <br>
   <button onclick='sjs.restore()' style="transition: ease 150ms; background: #46b063; border: none; border-radius: 3px; width: 500px; height: 50px;">Restore</button>
 </div>
 </div>
